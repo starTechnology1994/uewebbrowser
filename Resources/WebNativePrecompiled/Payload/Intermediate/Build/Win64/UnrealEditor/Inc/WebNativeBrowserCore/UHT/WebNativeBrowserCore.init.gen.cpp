a54a62dd3f8624aabacc593e11fdb031db2448e8
starTechnology1994 <startechnology1994@163.com>
2026-08-08 08:23:47 +0800

    UE5.3: update payload and CEF resources from FabRelease

--- Resources/WebNativePrecompiled/Payload/Intermediate/Build/Win64/UnrealEditor/Inc/WebNativeBrowserCore/UHT/WebNativeBrowserCore.init.gen.cpp
+++ Resources/WebNativePrecompiled/Payload/Intermediate/Build/Win64/UnrealEditor/Inc/WebNativeBrowserCore/UHT/WebNativeBrowserCore.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeWebNativeBrowserCore_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB9CD8CB6,
+				0x20B7DACD,
 				0x3A4687C6,
-				METADATA_PARAMS(nullptr, 0)
+				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_WebNativeBrowserCore.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_WebNativeBrowserCore.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WebNativeBrowserCore(Z_Construct_UPackage__Script_WebNativeBrowserCore, TEXT("/Script/WebNativeBrowserCore"), Z_Registration_Info_UPackage__Script_WebNativeBrowserCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB9CD8CB6, 0x3A4687C6));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WebNativeBrowserCore(Z_Construct_UPackage__Script_WebNativeBrowserCore, TEXT("/Script/WebNativeBrowserCore"), Z_Registration_Info_UPackage__Script_WebNativeBrowserCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x20B7DACD, 0x3A4687C6));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
