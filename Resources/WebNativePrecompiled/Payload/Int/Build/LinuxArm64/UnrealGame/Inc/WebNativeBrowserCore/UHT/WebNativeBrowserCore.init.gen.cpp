1d2c9e2353ed20bc96cb2430d0dc7b4147c81dae
starTechnology1994 <startechnology1994@163.com>
2026-08-16 21:15:56 +0800

    UE55: rebuild precompiled binaries - fix LNK2019 WebNativeJsonLibrary, add MarketplaceURL, add 3D widgets

--- Resources/WebNativePrecompiled/Payload/Int/Build/LinuxArm64/UnrealGame/Inc/WebNativeBrowserCore/UHT/WebNativeBrowserCore.init.gen.cpp
+++ Resources/WebNativePrecompiled/Payload/Int/Build/LinuxArm64/UnrealGame/Inc/WebNativeBrowserCore/UHT/WebNativeBrowserCore.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeWebNativeBrowserCore_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xD28ADE24,
+				0x303FD171,
 				0x027B2049,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeWebNativeBrowserCore_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_WebNativeBrowserCore.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WebNativeBrowserCore(Z_Construct_UPackage__Script_WebNativeBrowserCore, TEXT("/Script/WebNativeBrowserCore"), Z_Registration_Info_UPackage__Script_WebNativeBrowserCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD28ADE24, 0x027B2049));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WebNativeBrowserCore(Z_Construct_UPackage__Script_WebNativeBrowserCore, TEXT("/Script/WebNativeBrowserCore"), Z_Registration_Info_UPackage__Script_WebNativeBrowserCore, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x303FD171, 0x027B2049));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
